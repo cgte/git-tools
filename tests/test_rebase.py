3dc080246d74ec8fcb9ea9ebeffd9be4df1fcc1f
Colin Goutte <cgte@bk.ru>
2016-12-07 17:51:25 +0100

    Implement first test case

--- tests/test_rebase.py
+++ tests/test_rebase.py
@@ -18,14 +18,18 @@ from subprocess import check_call
 from os.path import splitext, abspath, dirname, join
 import os
 
-print __name__, __file__
-print splitext(__file__)
+from gittools import gitcmd
+
 directory = abspath(dirname(__file__))
 
-print directory
 
+devnull = open(os.devnull, 'w')
 
-
+def one(iterable):
+    content = list(iterable)
+    if len(iterable) != 1:
+        raise ValueError("More than one element in %r" % content)
+    return content[0]
 
 def rand(N=4):
     return '0000'
@@ -40,29 +44,42 @@ class DefaultTestCase(TestCase):
 
     def setUp(self):
         self.repodir = join(directory, 'repo_rebase_testbase_%s' % rand())
-        check_call('rm -rf %s' %  self.repodir, shell=True)
         os.mkdir(self.repodir)
 
     def test_base(self):
         os.chdir(self.repodir)
+        target = 'targetbranch'
         statements = ['git init ',
-                      'git checkout -b targebranch',
+                      'git checkout -b %s' % target,
                       'touch file1',
                       'git add file1',
                       "git commit -m 'add file1' file1",
                       "echo 'abce' > file1",
                       "git commit -m 'fill file1' file1",
                       "git checkout HEAD^",
-                      'git checkout -b testbranch',
+                      'git checkout -b branch_to_rebase',
                       "echo 'abce' > file2",
                       "git add file2",
                       "git commit -m 'add file to be rebased'",
+                      # Go back on working branch
+                      "git checkout %s" % target,
+                      "git checkout -b broader",
+                      "echo 'some content' >> file1 ",
+                      "git commit -m 'some heavy work on file 1' file1"
                       ]
-        print os.getcwd()
         for statement in statements:
-            print repr(statement)
-            check_call(statement, shell=True)
+            check_call(statement, shell=True, stdout=devnull, stderr=devnull)
 
+        self.assertEqual(one(gitcmd.broader_than(target)), 'broader')
+
+        self.assertEqual(gitcmd.unmerged(target), set(['broader', 'branch_to_rebase']))
+
+        self.assertEqual(one(gitcmd.unmerged(target) - gitcmd.broader_than(target)), 'branch_to_rebase')
+
+    def tearDown(self):
+        from time import sleep
+        sleep(2)
+        check_call('rm -rf %s' %  self.repodir, shell=True)
 
 if __name__ == '__main__':
     main()
