5d164524cdbf54d8fdd6c7677c15158e4b7658c3
Colin Goutte <cgte@bk.ru>
2017-01-03 12:52:30 +0100

    Allow to provide only one branch to rebase + debuginfo

--- gittools/autorebase.py
+++ gittools/autorebase.py
@@ -15,6 +15,8 @@ from gitcmd import sync_branch, unmerged,  broader_than
 
 import argparse
 
+import logging as log
+
 parser = argparse.ArgumentParser('Automatic rebaser')
 parser.add_argument('--sync-target', '-s', action='store_true',
                     help="Pulls and pushes the target "
@@ -23,21 +25,28 @@ parser.add_argument('--target-branch', '-t', default='master',
                     help="name of the branch to rebase onto"
                     )
 
+parser.add_argument('--branch', '-b', default=None,
+                    help="specify only one branch to rebase")
+
+
+
 def shouldnot_rebase_branch(branch_name):
     return 'norebase' in branch_name
 
 def main():
      params = parser.parse_args()
-     _main(target=params.target_branch, sync_target=params.sync_target)
+     _main(target=params.target_branch, sync_target=params.sync_target,
+           branch=params.branch)
 
-def _main(target='master', sync_target=True):
+def _main(target='master', sync_target=True, branch=None):
     if sync_target:
         sync_branch(target)
     diverged = unmerged(branch=target)
-
+    log.warn("Diverged \n %r" % list(diverged))
     broader = broader_than(branch=target)
+    log.warn("Broader \n %r" % list(broader))
 
-    to_rebase = diverged - broader
+    to_rebase = diverged - broader if not branch else [branch,]
 
     print 'branches to be rebased: ', ' ,'.join(to_rebase)
 
@@ -46,6 +55,7 @@ def _main(target='master', sync_target=True):
     for branch in to_rebase:
         if shouldnot_rebase_branch(branch):
             continue
+        print 'rebasing ', branch
         try:
             check_call(['git rebase %s %s' % (target, branch)],
                        shell=True)
@@ -58,4 +68,4 @@ def _main(target='master', sync_target=True):
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
